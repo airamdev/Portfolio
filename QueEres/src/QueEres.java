91b95e572b4393f180aa7640dc7ad0f39bca10d8
airamdev <airan-tg@hotmail.es>
2025-02-27 17:58:01 +0000

    Se optimizó el código del ejercicio 'QueEres'.

--- QueEres/src/QueEres.java
+++ QueEres/src/QueEres.java
@@ -1,31 +1,30 @@
+import java.time.Year;
 import java.util.Scanner;
 
 public class QueEres {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int thisYear = 2025;
-        System.out.println("Por favor, me puedes decir tu año de nacimiento: ");
-        int birthYear = sc.nextInt();
-        int age;
+        int thisYear = Year.now().getValue();
 
-        if (birthYear>thisYear) {
+        System.out.println("Por favor, dime tu año de nacimiento: ");
+        int birthYear = sc.nextInt();
+
+        if (birthYear > thisYear) {
             System.out.println("El año especificado no es posible");
-        } else if (birthYear<1970) {
-            age = thisYear - birthYear;
-            System.out.println ("Eres un@ pipiol@ de ... " + age + " años");
-        } else if (birthYear>=1970 && birthYear<=1980) {
-            age = thisYear - birthYear;
-            System.out.println ("Eres un@ baby boomeer... de ... " + age + " años");
-        } else if (birthYear>=1980 && birthYear<1990) {
-            age = thisYear - birthYear;
-            System.out.println ("Eres un@ ochenter@... de ... " + age + " años");
-        } else if (birthYear>=1980 && birthYear<1990) {
-            age = thisYear - birthYear;
-            System.out.println ("Eres un@ finalist@... del siglo XX ... " + age + " años");
         } else {
-            age = thisYear - birthYear;
-            System.out.println ("Eres un@ pipiol@... de ... " + age + " años");
+            int age = thisYear - birthYear;
+            String generacion = getGeneracion(birthYear);
+            System.out.printf("Eres un@ %s de %d años%n", generacion, age);
         }
+
+        sc.close();
+    }
+
+    private static String getGeneracion(int birthYear) {
+        if (birthYear < 1970) return "pipiol@";
+        if (birthYear <= 1980) return "baby boomer";
+        if (birthYear < 1990) return "ochenter@";
+        if (birthYear < 2000) return "finalist@ del siglo XX";
+        return "pipiol@";
     }
 }
